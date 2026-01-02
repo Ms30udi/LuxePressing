7089c5acfb306633cd60aebf7c402da0a17ef29f
medghazouan <ghezouanmohamed02@gmail.com>
2026-01-02 16:03:08 +0100

    base project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <main className="min-h-screen bg-black p-12">
+      <h1 className="text-teal-500 text-5xl font-semibold">
+        Pressing à Marrakech & Tanger
+      </h1>
+
+      <p className="mt-4 max-w-2xl text-gray-100 text-lg">
+        La référence du Pressing haute qualité Marrakech et Tanger.
+      </p>
+    </main>
+  );
+}
